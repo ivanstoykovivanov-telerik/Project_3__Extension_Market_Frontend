fa8fe80e3a0bcf0bb711213816e3ca24f7d12538
ivanstoykovivanov <ivanstoykovivanov@gmail.com>
2018-08-19 17:21:43 +0300

    Matching passwords validation added

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user.model';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -22,30 +21,43 @@ export class RegisterComponent implements OnInit {
             username: ['', Validators.minLength(6)],
             email: ['', [ Validators.email] ],
             password: ['', [ Validators.minLength(6)]],
-            confirm_password: ['', [ Validators.minLength(6)]]
+            confirm_password: ['', [ Validators.minLength(6), this.passValidator]]
         });
     }
  
-    // convenience getter for easy access to form fields
+    // a getter for easy access to form fields
     get f() { return this.registerForm.controls; }
     
 
-    // TODO  ADD
-    // passwordMatchValidator(g: FormGroup){
-    //   return g.get('password').value === g.get('rep_password').value
-    //   ? null : {'mismatch': true};
-    // }
+    //confirm if passwords match 
+    passValidator(control: AbstractControl) {
+        if (control && (control.value !== null || control.value !== undefined)) {
+            const cnfpassValue = control.value;
+    
+            const passControl = control.root.get('password'); // magic is this
+            if (passControl) {
+                const passValue = passControl.value;
+                if (passValue !== cnfpassValue || passValue === '') {
+                    return {
+                        isError: true
+                    };
+                }
+            }
+        }
+        return null;
+    }
 
 
     onSubmit() {
       this.submitted = true;
       console.log('SUCCESSFULLY REGISTERED ');
-       
+      
+
         // stop here if form is invalid
         if (this.registerForm.invalid) {
           console.log("Invalid registration form");
           return;
         }
- 
     }
+
 }
