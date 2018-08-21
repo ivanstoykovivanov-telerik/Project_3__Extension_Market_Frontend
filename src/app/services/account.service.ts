f312f97000517099c9a81c48bb69ad68398e7ff2
ivanstoykovivanov <ivanstoykovivanov@gmail.com>
2018-08-21 13:08:38 +0300

    Account service(used for registration )has been  added

---
+++ src/app/services/account.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
+import { AppComponent } from '../app.component';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class AccountService {
+  constructor(public http: HttpClient) { }
+
+  createAccount(user:User){
+    return this.http.post(AppComponent.API_URL+'/account/register',user) ; 
+      // .subscribe(resp =>resp.json());
+  }
+}
+
