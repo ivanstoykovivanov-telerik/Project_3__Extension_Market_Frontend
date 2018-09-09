cf9337c9388c953badba8efb1a8552d369f88abe
ivanstoykovivanov <ivanstoykovivanov@gmail.com>
2018-09-09 03:30:25 +0300

    Search service added

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
+import { AppComponent } from '../app.component';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
 
   public search(prouctName: string){
     return "Product Name"; 
   }
+
+  searchProducts(searched: string){
+    console.log("searched");
+    return this.http.post(`${AppComponent.API_URL}/search`, searched); 
+  }
+
 }
