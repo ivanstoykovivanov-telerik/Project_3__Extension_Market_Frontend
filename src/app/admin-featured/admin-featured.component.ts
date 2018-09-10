5c80dccb2d52394ecb490e0cf0b627a2e5202ab2
ivanstoykovivanov <ivanstoykovivanov@gmail.com>
2018-09-10 04:08:17 +0300

    ADMIN panel ready

--- src/app/admin-featured/admin-featured.component.ts
+++ src/app/admin-featured/admin-featured.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product.model';
+import { AdminService } from '../services/admin.service';
+import { UserService } from '../services/user.service';
+import { ProductService } from '../services/product.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-featured',
@@ -6,10 +11,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-featured.component.css']
 })
 export class AdminFeaturedComponent implements OnInit {
-
-  constructor() { }
+  
+  products : Product[] ; 
+  
+  constructor(
+      private adminService: AdminService, 
+      private userService: UserService, 
+      private productService: ProductService,
+      private router: Router, 
+  ) { }
 
   ngOnInit() {
+    
+    //get all products
+    this.adminService.getAllProducts()
+      .subscribe(data => {
+        console.log(data)
+        this.products = data
+      })
+  }
+
+
+  getStatus(product){
+    return product.featuredProduct ; 
+  }
+  
+ 
+  onFeatureProduct($event, product){
+    console.log("In");
+    if(product.featuredProduct === false ){
+      this.adminService.featureProduct(product)
+          .subscribe();
+      product.featuredProduct = !product.featuredProduct; 
+      return ; 
+    }
+    
+    if(product.featuredProduct === true ){
+      this.adminService.unFeatureProduct(product)
+          .subscribe();
+      product.featuredProduct = !product.featuredProduct; 
+        return ; 
+    }
+  }
+
+
+  showProduct(product){
+     this.router.navigate(['/productDetails', {id: product.id} ]);
   }
 
 }
